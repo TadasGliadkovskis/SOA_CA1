c7de796860f8ae0b7c566671114e77a812d86ad8
Ted <tadas2410@gmail.com>
2022-10-17 07:28:34 +0100

    added search and button

--- Tadas_Gliadkovskis_CA1/Form1.cs
+++ Tadas_Gliadkovskis_CA1/Form1.cs
@@ -19,7 +19,7 @@ namespace Tadas_Gliadkovskis_CA1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void getAllButton_Click(object sender, EventArgs e)
         {
             var client = new RestClient("https://www.fruityvice.com/api/fruit/banana");
             var request = new RestRequest();
@@ -32,33 +32,5 @@ namespace Tadas_Gliadkovskis_CA1
         }
 
 
-
-        private void InitializeComponent()
-        {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.LightGreen;
-            this.button1.BackgroundImage = global::Tadas_Gliadkovskis_CA1.Properties.Resources.dots_image1;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.OrangeRed;
-            this.button1.Location = new System.Drawing.Point(113, 41);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 48);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Get all Fruits";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
-            this.ClientSize = new System.Drawing.Size(690, 473);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.ResumeLayout(false);
-
-        }
     }
 }
